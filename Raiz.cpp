f2657da1861292df7053187c88722c8dfae6a526
marlenshh <marlen765shn@gmail.com>
2021-03-25 10:58:22 -0600

    Correción final

--- Raiz.cpp
+++ Raiz.cpp
@@ -1,12 +1,9 @@
-#include<iostream>
-#include <math.h>
-using namespace std;
-
 int main(){
 float a=0;
 float b=0;
 float c=0;
-float resultado=0;
+float resultado1=0;
+float resultado2=0;
 
 cout<<"Inserte los valores de sus variables a, b y c respectivamente"<<endl;
 cin>>a;
@@ -16,11 +13,14 @@ cin>>c;
 float root=pow(b,2)-(4*a*c);
 if(a>0){
     if(root>0){
-        resultado=(((-1)*b)-(sqrt(root))/(2*a));
-        cout<<"El resultado de la raiz es "<<resultado<<endl;
+        resultado1=(((-1)*b)-(sqrt(root))/(2*a));
+        resultado2=(((-1)*b)+(sqrt(root))/(2*a));
+        cout<<"El resultado de la ecuación es "<<resultado1<<endl;
+        cout<<"El resultado de la ecuación es "<<resultado2<<endl;
     }else cout<<"Tu operación tiene una raiz imaginaria"<<endl;
 }else cout<<"Tu operacion tiene una division sobre 0"<<endl;
 
 
-
 }
+
+
